787414855a172867821e50673ab8080129da36fe
PreethiDhal <preethidhal06@gmail.com>
2024-12-24 10:57:49 +0530

    Objects

---
+++ JSONObject.js
@@ -0,0 +1,20 @@
+// s1 : name , age
+// s2 : name , email
+// s3: name , Work_Experience
+const s1 = {
+ name : "priti",
+ age : 22,
+}
+
+const s2 = {
+    name : "preethi",
+    email : "preethi@123gmail.com",
+}
+
+const s3 = {
+    name : "dhal",
+    Work_Experience : {
+        xyz : "2months",
+        abc : "12months",
+    }
+}
