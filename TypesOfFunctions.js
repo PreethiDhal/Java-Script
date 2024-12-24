2c31d5e5cb63e2302692e41d9862d7eb0142c7a8
PreethiDhal <preethidhal06@gmail.com>
2024-12-24 09:58:18 +0530

    Diff-function-declaration

--- TypesOfFunctions.js
+++ TypesOfFunctions.js
@@ -3,11 +3,13 @@ function greet1(name){
     return `Hello,${name}`
 }
 console.log(greet1('Priya'))
+
 //Function Expression: Not Hoisted
 let greet2 = function(name){
     return `Hello,${name}`
 }
 console.log(greet2('Ankit'))
+
 //Arrow Function: Callbacks
 let greet3 = (name)=>{
     return `Hello,${name}`
