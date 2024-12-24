6718dbed267774e287349d6396b9cbaf0a42f597
PreethiDhal <preethidhal06@gmail.com>
2024-12-24 10:10:22 +0530

    DataTypes

---
+++ DataTypes.js
@@ -0,0 +1,19 @@
+// Number Data Type: All numbers (whole,decimal,positive,negative)
+var a = 456/0;
+console.log(a, typeof a);
+
+// Strings: single character or series of characters all are strings
+var b = 'kodnest'
+console.log(b, typeof b);
+
+// boolean: true / false
+var c = true;
+console.log(c,typeof c);
+
+// undefined
+let d;
+console.log(d);
+
+// null
+let e = null;
+console.log(e);
