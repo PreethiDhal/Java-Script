2efe9e849b561da9fee0fcb5eebb386c32e5fe2f
PreethiDhal <preethidhal06@gmail.com>
2025-01-02 10:50:01 +0530

    Hoisting

--- Hoisting.js
+++ Hoisting.js
@@ -1,9 +1,14 @@
-console.log(a);
-const a = 30;
-console.log(a);
-console.log(b);
-let b = 60;
-console.log(b);
-console.log(c);
-var c = 40;
-console.log(d);
+add()
+sub()
+function sub() {
+   // console.log(x+y);
+    let x = 40;
+    var y = 40;
+    console.log(x+y);
+}
+function add() {
+    console.log(a+b);
+    var a = 10;
+    var b = 20;
+    console.log(a+b);
+}
